269daa3bfe6e557675d00d021de8b252d7c072df
조혜정 <nxlab@naver.com>
2026-03-23 16:47:31 +0900

    chore: configure build for Vercel + PostgreSQL deployment

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,20 +1,8 @@
-// schema v2 - StepConnection, parentStepId, stepId
-// Import from client-new to get fresh Turbopack compilation
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const { PrismaClient } = require("../../node_modules/.prisma/client-new/index.js");
-
-const SCHEMA_VERSION = "v3";
+import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
-  prismaSchemaVersion: string | undefined;
 };
-
-// Reset singleton if schema version changed
-if (globalForPrisma.prismaSchemaVersion !== SCHEMA_VERSION) {
-  globalForPrisma.prisma = undefined;
-  globalForPrisma.prismaSchemaVersion = SCHEMA_VERSION;
-}
 
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 
